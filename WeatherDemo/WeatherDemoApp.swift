1fe5a7a99498a42fe525878cfaf104410fbe8571
ZUH4Ir <zuhairlakhani@gmail.com>
2022-10-30 01:59:45 -0500

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Zuhair Lakhani on 10/30/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
